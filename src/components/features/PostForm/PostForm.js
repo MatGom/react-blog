cf7ead988af839e491379ea056ea5bf0d458d9c2
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-04-12 01:26:18 +0100

    Add PostForm

---
+++ src/components/features/PostForm/PostForm.js
@@ -0,0 +1,82 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const PostForm = ({ action, actionText, ...props }) => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState(props.title || '');
+  const [author, setAuthor] = useState(props.author || '');
+  const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
+  const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
+  const [content, setContent] = useState(props.content || '');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    action({ title, author, publishedDate, shortDescription, content });
+    navigate('/');
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className='mb-3 w-50'>
+        <Form.Label>{actionText}</Form.Label>
+        <Form.Control
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          type='text'
+          placeholder='Enter title'
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3 w-50'>
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          value={author}
+          onChange={event => setAuthor(event.target.value)}
+          type='text'
+          placeholder='Enter author'
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3 w-50'>
+        <Form.Label>Published date</Form.Label>
+        <Form.Control
+          value={publishedDate}
+          onChange={event => setPublishedDate(event.target.value)}
+          type='text'
+          placeholder='Enter date'
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3'>
+        <Form.Label>Short description</Form.Label>
+        <Form.Control
+          value={shortDescription}
+          onChange={event => setShortDescription(event.target.value)}
+          as='textarea'
+          placeholder='Leave a comment here'
+          style={{ height: '100px' }}
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3'>
+        <Form.Label>Main content</Form.Label>
+        <Form.Control
+          value={content}
+          onChange={event => setContent(event.target.value)}
+          as='textarea'
+          placeholder='Leave a comment here'
+          style={{ height: '100px' }}
+        />
+      </Form.Group>
+
+      <Button variant='primary' type='submit' className='mb-3'>
+        {action.text}
+      </Button>
+    </Form>
+  );
+};
+
+export default PostForm;
